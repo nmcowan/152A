9a65b6bab781491b6e8fa91e21a5140e1acc5b1c
Natalie Cowan <nmcowan@ucdavis.edu>
2018-04-30 21:36:56 -0700

    All bugs fixed

--- ip2as.cpp
+++ ip2as.cpp
@@ -21,57 +21,70 @@ int main(int argc, char *argv[]){
 
 	while(getline(IP,ipAddress)){
 		orig_ipAddress = ipAddress.substr(0, ipAddress.length());
-		int posIP = ipAddress.find(".");
-		vector<string> longestPrefix;
+
+		vector<string> addressIP, longestPrefix;
 		vector<int> maskVector;
 		maskVector.push_back(0);
 
+		typedef boost::tokenizer<boost::char_separator<char>> tokenizerIP;
+  		boost::char_separator<char> sep{"."};
+  		tokenizerIP tokIP{ipAddress, sep};
+  		for (const auto &t : tokIP)
+  			addressIP.push_back(t);
+
+
 		DataBase.open(argv[1]);
 
+		while(getline(DataBase, temp,'\n')){
+			//parse each line into appropriate field
 
-		while(DataBase >> dataIP >> mask >> asNum){
+			stringstream parse(temp);
+			parse >> dataIP >> mask >> asNum;
 			orig_dataIP = dataIP.substr(0,dataIP.length());
-			vector<string> addressIP, addressDB;
+
+
+			//Create vector to store each byte of IP address
+			vector<string> addressDB;
+
 			int maskNum = atoi(mask.c_str());
 			int orig_maskNum = maskNum;
 
-			if(maskNum < maskVector.back()){
-				break;
-			}
-
-			typedef boost::tokenizer<boost::char_separator<char>> tokenizerIP;
-  			boost::char_separator<char> sep{"."};
-  			tokenizerIP tokIP{ipAddress, sep};
-  			for (const auto &t : tokIP)
-  				addressIP.push_back(t);
-
+			//Parse the IP address by "."
   			typedef boost::tokenizer<boost::char_separator<char>> tokenizerDB;
   			boost::char_separator<char> sepDB{"."};
   			tokenizerDB tokDB{dataIP, sepDB};
   			for (const auto &t : tokDB)
   				addressDB.push_back(t);
 
-			//if first digits match 
-			if(!addressDB[0].compare(addressIP[0])){
+			//If the first byte matches and the current mask is greater than the last
+			if( (!addressDB[0].compare(addressIP[0])) && (maskNum > maskVector.back()) ){
 				maskNum -= 8;
+
+				//For the other 3 bytes in the IP address
 				for(int i = 1; i <= 3; i++){
+					//If the mask is greater than 8 and the 2nd bytes are equal
 					if(maskNum >= 8 && !addressIP[i].compare(addressDB[i])){
 						maskNum -= 8;
+						continue;
 					} else if(maskNum >= 8 && addressIP[i].compare(addressDB[i])){
 						break;
 					}
 
-					if(maskNum < 8){
+					//If the mask is now less than 8
+					if( (maskNum < 8)){
+						//Convert strings to ints and shift the byte 
 						int ipNum = atoi(addressIP[i].c_str());
 						int dbNum = atoi(addressDB[i].c_str());
 
 						int shiftIP = ipNum >> (8 - maskNum);
 						int shiftDB = dbNum >> (8 - maskNum);
 
-
 						if(shiftDB == shiftIP){
+
+							//push the current mask 
 							maskVector.push_back(orig_maskNum);
 
+							//Add for final output
 							dataIP.append("/");
 							dataIP.append(mask);
 							dataIP.append(" ");
@@ -79,7 +92,7 @@ int main(int argc, char *argv[]){
 							dataIP.append(" ");
 							dataIP.append(orig_ipAddress);
 							longestPrefix.push_back(dataIP);
-							continue;
+							break;
 						}
 					}
 
